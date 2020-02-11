090b0d0de8d60dbed4ec0f91a480bbb8955649b8
Dizzled <dustin.lagrone@gmail.com>
2020-02-11 13:55:45 -0800

    Adding morefunction

--- Section9/morefunction.py
+++ Section9/morefunction.py
@@ -1,25 +1,43 @@
 try:
     import tkinter
+    import math
 except ImportError: #python 2
     import Tkinter as tkinter
 
 
-def parabola(x):
-    y = x * x / 100
-    return y
+def parabola(page, size):
+    for x in range(-size, size):
+        y = x * x / size
+        plot(page, x, y)
+        plot(page, -x,y)
 
 
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill='white')
-    canvas.create_line(0, y_origin, 0, -y_origin, fill='white')
+def circle(page, g, h, radius, outline='red'):
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill='red')
+    page.create_oval(g + radius, h + radius, g - radius, h - radius, outline=outline, width=2)
+    # for x in range(g * 100, (g + radius) * 100):
+    #     x /= 100
+    #     y = h + (math.sqrt(radius ** 2 - ((x - g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2 * h - y)
+    #     plot(page, 2 * g - x, y)
+    #     plot(page, 2 * g - x, 2 * h - y)
+
+
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill='white')
+    page.create_line(0, y_origin, 0, -y_origin, fill='white')
+
+    #print(locals()) # Prints the lccal variables inside the function
+
+
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill='red')
 
 
 mainWindow = tkinter.Tk()
@@ -31,8 +49,10 @@ canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
 draw_axes(canvas)
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
+parabola(canvas, 100)
+parabola(canvas, 200)
 
+circle(canvas, 100, 100, 100, 'blue')
+circle(canvas, 80, 90, 100)
+circle(canvas, 100, 100, 100, 'black')
 mainWindow.mainloop()
