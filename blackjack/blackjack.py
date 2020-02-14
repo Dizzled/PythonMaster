8253d9cdae2d56b0d4daf6ecd6a97ccc731b2079
Dustin <dustin.lagrone@gmail.com>
2020-02-13 17:34:54 -0800

    Added BlackJack

---
+++ blackjack/blackjack.py
@@ -0,0 +1,15 @@
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+import random
+
+
+mainWindow = tkinter.Tk()
+
+#Set up the screen and frames for the dealer and player
+mainWindow.title("Black Jack")
+mainWindow.geometry("640x480")
+
+
