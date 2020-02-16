730b5f8b94ac835ea6f5f26a5cdf7ad284070149
Dizzled <dustin.lagrone@gmail.com>
2020-02-16 14:50:43 -0800

    BlackJack

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -6,10 +6,8 @@ except ImportError:
     import Tkinter as tkinter
 
 
-mainWindow = tkinter.Tk()
-
 def loadImages(cardImages):
-    suits=['heart', 'club', 'diamond', 'spade']
+    suits = ['heart', 'club', 'diamond', 'spade']
     facecards = ['jack', 'queen', 'king']
 
     if tkinter.TkVersion >= 8.6:
@@ -33,12 +31,104 @@ def loadImages(cardImages):
             cardImages.append((10, image,))
 
 
+def dealCard(frame):
+    # Pop the next card off the top of the deck use .pop(0) to do so
+    nextCard = deck.pop(0)
+    # add the image to a Label and display the label
+    tkinter.Label(frame, image=nextCard[1], relief='raised').pack(side='left')
+    # now return the card's face value
+    return nextCard
+
+
+def scoreHand(hand):
+    # Calculate the total score of all cards in the list.
+    # Only one ace can have the value 11, and this will be reduced to 1 if the hand would bust.
+    score = 0
+    ace = False
+    for nextCard in hand:
+        cardValue = nextCard[0]
+        if cardValue == 1 and not ace:
+            ace = True
+            cardValue = 11
+        score += cardValue
+        # if we would bust, check if there is an ace and subtract 10
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
+
+
+def dealDealer():
+
+    dealerScore = scoreHand(dealerHand)
+    while 0 < dealerScore < 17:
+        dealerHand.append(dealCard(dealerCardFrame))
+        dealerScore = scoreHand(dealerHand)
+        dealerScoreLabel.set(dealerScore)
+
+    playerScore = scoreHand(playerHand)
+    if playerScore > 21:
+        resultText.set("Dealer Wins!")
+    elif dealerScore > 21 or dealerScore < playerScore:
+        resultText.set('Player Wins!')
+    elif dealerScore > playerScore:
+        resultText.set("Dealer Wins!")
+    else:
+        resultText.set('Draw!')
+
+
+
+def dealPlayer():
+
+    playerHand.append(dealCard(playerCardFrame))
+    playerScore = scoreHand(playerHand)
+
+    playerScoreLabel.set(playerScore)
+    if playerScore > 21:
+        resultText.set('Dealer Wins!')
+    # global playerScore
+    # global playerAce
+    #
+    # cardValue = dealCard(playerCardFrame)[0]
+    # if cardValue == 1 and not playerAce:
+    #     playerAce = True
+    #     cardValue = 11
+    # playerScore += cardValue
+    # # If we would bust, check if there is an ace and subtract
+    # if playerScore > 21 and playerAce:
+    #     playerAce = False
+    #     playerScore -= 10
+    # playerScoreLabel.set(playerScore)
+    # if playerScore > 21:
+    #     resultText.set("Dealer Wins!")
+    #
+    # print(locals())
+
+
+def resetGame():
+
+    global deck
+    dealerHand.clear()
+    playerHand.clear()
+    playerScoreLabel.set('0')
+    dealerScoreLabel.set('0')
+
+    deck = list(cards)
+    random.shuffle(deck)
+    dealPlayer()
+    dealDealer()
+    dealerCardFrame()
+
+
+mainWindow = tkinter.Tk()
 # Set up the screen and frames for the dealer and player
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
+mainWindow.configure(background='green')
 
 resultText = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=resultText)
+result.grid(row=0, column=0, columnspan=3)
 
 cardFrame = tkinter.Frame(mainWindow, relief='sunken', borderwidth=1, background='green')
 cardFrame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
@@ -52,6 +142,7 @@ dealerCardFrame = tkinter.Frame(cardFrame, background='green')
 dealerCardFrame.grid(row=0, column=1, sticky='ew', rowspan=2)
 
 playerScoreLabel = tkinter.IntVar()
+
 tkinter.Label(cardFrame, text='Player', background='green', fg='white').grid(row=2, column=0)
 tkinter.Label(cardFrame, textvariable=playerScoreLabel, background='green', fg='white').grid(row=3, column=0)
 
@@ -63,12 +154,31 @@ playerCardFrame.grid(row=2, column=1, sticky='ew', rowspan=2)
 buttonFrame = tkinter.Frame(mainWindow)
 buttonFrame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealerButton = tkinter.Button(buttonFrame, text='Dealer')
+dealerButton = tkinter.Button(buttonFrame, text='Dealer', command=dealDealer)
 dealerButton.grid(row=0, column=0)
 
-playerButton = tkinter.Button(buttonFrame, text='Player')
+playerButton = tkinter.Button(buttonFrame, text='Player', command=dealPlayer)
 playerButton.grid(row=0, column=1)
 
+
+resetGame = tkinter.Button(buttonFrame, text='New Game', command=resetGame)
+resetGame.grid(row=0, column=2)
+
 # load cards
-card = []
-loadImages(card)
+cards = []
+loadImages(cards)
+# print(cards)
+
+# Create a new deck of cards and shuffle them
+deck = list(cards)
+random.shuffle(deck)
+
+# Create the list ot store the dealer's and player's hands
+dealerHand = []
+playerHand = []
+
+dealPlayer()
+dealerHand.append(dealCard(dealerCardFrame))
+dealerScoreLabel.set(scoreHand(dealerHand))
+dealPlayer()
+mainWindow.mainloop()
